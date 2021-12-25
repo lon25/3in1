fbcdd4bd808049c9222e45b319127103a35a8650
lon25 <80402174+lon25@users.noreply.github.com>
2021-12-25 23:41:04 +0700

    Update m-minerd.sh

--- m-minerd.sh
+++ m-minerd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd "$(dirname "$0")"
+cd m-minerd-64-linux
 
 chmod +x ./m-minerd -a m7mhash -o stratum+tcp://m7m.sea.mine.zpool.ca:6033 -u LTVqz5HkQuqGMBZEA1LVYPopRDcKT5DHTP -p c=LTC
 
